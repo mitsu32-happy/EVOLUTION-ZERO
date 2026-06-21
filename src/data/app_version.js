c16b9b098d641023cd785706dcd6e599e3a8b2f7
mitsu32-happy <oushiza32@gmail.com>
2026-06-21 17:19:22 +0900

    chore: release compsognathus minipack v1

--- src/data/app_version.js
+++ src/data/app_version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.12.0';
-export const APP_BUILD = 'stage4-zero-v1';
+export const APP_VERSION = '0.12.1';
+export const APP_BUILD = 'compsognathus-minipack-v1';
 export const APP_VERSION_LABEL = `VERSION ${APP_VERSION}`;
